5ad69b909797fa0fd6a01869c17a851bae9a9102
Neil <vitalene@msu.edu>
2016-09-21 14:46:07 -0400

    Initial Commit

---
+++ PrimesTest/PrimesTest.h
@@ -0,0 +1,19 @@
+//
+//  PrimesTest.h
+//  PrimesTest
+//
+//  Created by Neil Vitale on 9/21/16.
+//  Copyright © 2016 Neil Vitale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrimesTest.
+FOUNDATION_EXPORT double PrimesTestVersionNumber;
+
+//! Project version string for PrimesTest.
+FOUNDATION_EXPORT const unsigned char PrimesTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrimesTest/PublicHeader.h>
+
+
